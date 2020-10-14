7849ff0396bb5258c0dccfcef497dace7336a828
einzeloner <jhashrijit@protonmail.com>
2020-10-14 12:31:10 +0530

    Initialized repo

---
+++ pacinst.sh
@@ -0,0 +1,5 @@
+sudo pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server udisks2 gvfs gvfs-mtp pcmanfm alcritty kitty bashtop firefox kate evince youtube-dl xterm vlc vim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet discord kdeconnect awesome steam wine nvidia nvidia-dkms plasma-meta sddm 
+#starting of the exec codes
+echo "exec startpalsma-x11" >> ~/.xinitrc
+#systemctl service
+systemctl enable sddm.service
