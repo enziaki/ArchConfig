79528d359c1a9ea45cda355a2c5d1adb907eb5ab
Einzeloner <58030340+Einzeloner@users.noreply.github.com>
2020-11-12 00:37:06 +0530

    Updated repo name and some apps

---
+++ pacinst.sh
@@ -0,0 +1,9 @@
+sudo pacman -Sy pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server udisks2 gvfs gvfs-mtp pcmanfm alcritty kitty bashtop firefox kate evince youtube-dl xterm vlc vim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet discord kdeconnect awesome plasma-meta sddm sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh 
+#starting of the exec codes
+neofetch | lolcat
+echo "Now I will enable some services which will ask you for your password so be prepared UwU" | lolcat
+echo "exec startpalsma-x11" >> ~/.xinitrc
+#systemctl service
+systemctl enable sddm.service && systemctl start sddm
+echo "You need to enable multilib which will let you install steam and wine. It's in /etc/pacman.conf"
+
