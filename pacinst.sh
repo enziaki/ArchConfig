f0f8bbaad3350e75bc6bc72d8ce7f0f1952e413c
Shriaki <58030340+enziaki@users.noreply.github.com>
2021-01-31 07:26:44 +0530

    Added ST and DWm ins wgets

--- pacinst.sh
+++ pacinst.sh
@@ -1,6 +1,6 @@
 echo "The script will now ask you for your sudo password."
 sudo su
-pacman -Syyu pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc vim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect awesome plasma-meta sddm sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
+pacman -Syyu pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc vim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect awesome plasma-meta sddm sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
 #starting of the exec codes
 neofetch | lolcat
 echo "Now I will enable some services which will ask you for your password so be prepared UwU" | lolcat
@@ -14,9 +14,11 @@ touch ~/.zsh_history
 #Changing the shell to zsh from bash
 chsh -s /usr/bin/zsh
 #Installing dwm even though you have KDE installed
-echo "Downloading DWM in `pwd`. For installing refer to extra.txt"
+echo "Downloading DWM and St in `pwd`. For installation refer to extra.txt"
 wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
+wget https://dl.suckless.org/st/st-0.8.4.tar.gz
 tar -xpvf dwm-6.2
+tar -xpvf st-0.8.4
 #just extracting because dwm may require some xorg headers that may or may not be installed on your system
 #For installing refer to less.txt
 exit
