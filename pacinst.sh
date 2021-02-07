3d07b3b6152fd36402233f70789959f550370a1c
Enziaki <jhashrijit@protonmail.com>
2021-02-07 21:11:03 +0530

    Editing config according to my preferences

--- pacinst.sh
+++ pacinst.sh
@@ -1,12 +1,10 @@
 echo "The script will now ask you for your sudo password."
 sudo su
-pacman -Syyu pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc vim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect awesome plasma-meta sddm sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
+pacman -Syyu pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc gvim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
 #starting of the exec codes
 neofetch | lolcat
 echo "Now I will enable some services which will ask you for your password so be prepared UwU" | lolcat
-echo "exec startpalsma-x11" >> ~/.xinitrc
 #systemctl service
-systemctl enable sddm
 echo "You need to enable multilib which will let you install steam and wine. It's in /etc/pacman.conf" | lolcat
 echo "Current user: `whoami`"
 cp .zshrc ~/
