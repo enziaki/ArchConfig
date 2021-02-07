9990e55b9fffce9c30c1e35d728e02f61b03e7cf
Enziaki <jhashrijit@protonmail.com>
2021-02-07 21:20:34 +0530

    Another random commit

--- pacinst.sh
+++ pacinst.sh
@@ -1,14 +1,15 @@
 echo "The script will now ask you for your sudo password."
 sudo su
-pacman -Syyu pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc gvim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
+pacman -Syyu pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia nvidia-prime nmcli nvidia-utils nvidia-settings mesa-demos wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc gvim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
 #starting of the exec codes
 neofetch | lolcat
 echo "Now I will enable some services which will ask you for your password so be prepared UwU" | lolcat
 #systemctl service
 echo "You need to enable multilib which will let you install steam and wine. It's in /etc/pacman.conf" | lolcat
 echo "Current user: `whoami`"
-cp .zshrc ~/
-touch ~/.zsh_history
+# Will not be needing the next two lines for my install.
+#cp .zshrc ~/
+#touch ~/.zsh_history
 #Changing the shell to zsh from bash
 chsh -s /usr/bin/zsh
 #Installing dwm even though you have KDE installed
