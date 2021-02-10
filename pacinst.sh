551bf2c03cc79f8545695807ebe4a2070adb6824
Enziaki <jhashrijit@protonmail.com>
2021-02-10 20:04:59 +0530

    A finished build of DWM with install script

--- pacinst.sh
+++ pacinst.sh
@@ -1,6 +1,4 @@
-echo "The script will now ask you for your sudo password."
-sudo su
-pacman -Syyu xorg-xrandr xorg-xsetroot pamixer ttf-font-awesome pavucontrol pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia nvidia-prime nmcli nvidia-utils nvidia-settings mesa-demos wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc gvim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
+sudo pacman -Syyu xorg-xrandr xorg-xsetroot rust cargo-bloat pamixer ttf-font-awesome pavucontrol pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia nvidia-prime nvidia-utils nvidia-settings mesa-demos wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc gvim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
 #starting of the exec codes
 neofetch | lolcat
 echo "Now I will enable some services which will ask you for your password so be prepared UwU" | lolcat
@@ -11,13 +9,13 @@ echo "Current user: `whoami`"
 #cp .zshrc ~/
 #touch ~/.zsh_history
 #Changing the shell to zsh from bash
-chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh
 #Installing dwm even though you have KDE installed
-echo "Downloading DWM and St in `pwd`. For installation refer to extra.txt"
+echo "Downloading DWM and ST in `pwd`. For installation refer to extra.txt"
 wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
 wget https://dl.suckless.org/st/st-0.8.4.tar.gz
-tar -xpvf dwm-6.2
-tar -xpvf st-0.8.4
+tar -xzvf dwm-6.2.tar.gz
+tar -xzvf st-0.8.4.tar.gz
 #just extracting because dwm may require some xorg headers that may or may not be installed on your system
 #For installing refer to extra.txt
 exit
