7b2e2bbef443444122bad1c8cd6c95da8cc6bb75
enziaki <58030340+enziaki@users.noreply.github.com>
2021-04-09 06:10:39 +0530

    Fixed the installer sctipy

--- pacinst.sh
+++ pacinst.sh
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
+
 sudo pacman -Syyu xorg-xrandr xorg-xsetroot nvidia-dkms nomacs rust xfce4-power-manager xfce4-screenshooter cargo-bloat ttf-font-awesome pavucontrol volumeicon awesome feh pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia nvidia-prime nvidia-utils nvidia-settings mesa-demos wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc gvim unzip file-roller unrar thermald tlp scrot rofi nitrogen picom ntfs-3g alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
-=======
-sudo pacman -Syyu xorg-xrandr xorg-xsetroot rust xfce4-power-manager xfce4-screenshooter cargo-bloat pamixer ttf-font-awesome pavucontrol awesome feh pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia nvidia-prime nvidia-utils nvidia-settings mesa-demos wget udisks2 gvfs gvfs-mtp pcmanfm kitty bashtop firefox youtube-dl xterm vlc gvim unzip unrar thermald tlp scrot rofi nitrogen picom ntfs-3g gtkimageview alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst 
->>>>>>> 0064818 (Commitment from a new directory)
+
 #starting of the exec codes
 neofetch | lolcat
 echo "Now I will enable some services which will ask you for your password so be prepared UwU" | lolcat
 echo "You need to enable multilib which will let you install steam and wine. It's in /etc/pacman.conf" | lolcat
 echo "Current user: `whoami`"
-# Will not be needing the next two lines for my install.
-<<<<<<< HEAD
 cp .zshrc ~/
 touch ~/.zsh_history
-#Changing the shell to zsh from bash
-sudo chsh -s $(which zsh)
+# Changing the shell to zsh from bash
+# sudo chsh -s $(which zsh)
 
 echo "Downloading DWM and ST in `pwd`. For installation refer to extra.txt"
 wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
@@ -25,20 +21,4 @@ git clone https://aur.archlinux.org/yay-git.git
 #just extracting because dwm may require some xorg headers that may or may not be installed on your system
 #For installation refer to extra.txt
 less extra.txt
-=======
-# cp .zshrc ~/
-# touch ~/.zsh_history
-#Changing the shell to zsh from bash
-# sudo chsh -s $(which zsh)
-
-# echo "Downloading DWM and ST in `pwd`. For installation refer to extra.txt"
-# wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
-# wget https://dl.suckless.org/st/st-0.8.4.tar.gz
-# tar -xzvf dwm-6.2.tar.gz
-# tar -xzvf st-0.8.4.tar.gz
-#just extracting because dwm may require some xorg headers that may or may not be installed on your system
-#For installing refer to extra.txt
-exit
-# less extra.txt
->>>>>>> 0064818 (Commitment from a new directory)
 
