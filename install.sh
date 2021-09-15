d69061b3ffa82507bc97ad039b60adb4c0ee3505
Enziaki <58030340+enziaki@users.noreply.github.com>
2021-09-15 22:03:08 +0530

    Some more fonts

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
-sudo pacman -Syyu xorg-xrandr xorg-xsetroot nvidia-dkms parcellite nomacs rust xfce4-power-manager xfce4-screenshooter cargo-bloat ttf-font-awesome pavucontrol volumeicon awesome feh pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia nvidia-prime nvidia-utils nvidia-settings mesa-demos wget udisks2 gvfs gvfs-mtp nautilus kitty bashtop firefox youtube-dl xterm vlc gvim unzip file-roller unrar thermald tlp scrot rofi nitrogen picom ntfs-3g alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst chromium code htop java8-openjfx jdk8-openjdk jre8-openjdk jre8-openjdk-headless lib32-nvidia-utils mpv network-manager-applet noto-fonts-emoji sxiv aarch64-linux-gnu-gcc arm-none-eabi-gcc blueberry xfce4-settings
+sudo pacman -Syyu xorg-xrandr xorg-xsetroot nvidia-dkms parcellite nomacs rust xfce4-power-manager xfce4-screenshooter cargo-bloat ttf-font-awesome pavucontrol volumeicon awesome feh pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server steam wine nvidia nvidia-prime nvidia-utils nvidia-settings mesa-demos wget udisks2 gvfs gvfs-mtp nautilus kitty bashtop firefox youtube-dl xterm vlc gvim unzip file-roller unrar thermald tlp scrot rofi nitrogen picom ntfs-3g alsa-utils neofetch lolcat handbrake gimp figlet playerctl kdeconnect sshfs ttf-sazanami discord zsh acpi python lxappearance libreoffice-fresh dunst chromium code htop java8-openjfx jdk8-openjdk jre8-openjdk jre8-openjdk-headless lib32-nvidia-utils mpv network-manager-applet noto-fonts-emoji sxiv aarch64-linux-gnu-gcc arm-none-eabi-gcc blueberry xfce4-settings ttf-fira-sans ttf-fira-code otf-fira-mono otf-fantasque-sans-mono ttf-fantasque-sans-mono
 
 neofetch | lolcat
 echo "Now I will enable some services which will ask you for your password so be prepared" | lolcat
@@ -13,13 +13,21 @@ touch ~/.zsh_history
 chsh -s $(which zsh)
 
 #Copying the config files to their destined places
-mkdir ~/.config/awesome/ ~/.config/picom/
+mkdir ~/.config/awesome/ ~/.config/picom/ ~/.confif/kitty/
 
 #Copying the awesome dotfile (just the config because theme is optional)
 cp awesome/rc.lua ~/.config/awesome/
 
 #copying the picom config
 cp picom/picom.conf ~/.config/picom/
+
+#copying the kitty config and changing the color to gruvbox
+cp kitty/kitty.conf ~/.config/kitty/
+cp kitty/gruvbox.conf ~/.config/kitty/
+ln -sf ~/.config/kitty/gruvbox.conf ~/.config/kitty/theme.conf
+
+#Vim and it's files
+cp vim/.vimrc ~/
 
 echo "Downloading DWM and ST in `pwd`. For installation refer to extra.txt"
 wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
@@ -34,5 +42,6 @@ git clone https://aur.archlinux.org/yay-git.git
 
 #For installation refer to extra.txt
 echo "Opening more install instructions..."
+echo "You might want to reboot as the nvidia drivers are installed now!" | lolcat
 less extra.txt
 
